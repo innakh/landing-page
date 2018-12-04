c07ff8b5256051d87138cc0d45b1dfb09c3bbcaa
innakh <innabakurina@icloud.com>
2018-12-05 00:48:33 +0200

    first commit

---
+++ www/js/init.js
@@ -0,0 +1,12 @@
+$(function() {
+  $( '.tabs' ).tabs();
+  $( '.portfolio-tab' ).tabs();
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    dots:true,
+    items:1
+      
+  })
+  
+});
